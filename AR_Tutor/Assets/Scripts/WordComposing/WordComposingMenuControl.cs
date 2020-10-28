b1f1c669c0ebe2d9586764074749f2fcbc572fac
Anton <akarnomazov@gmail.com>
2020-10-28 10:37:57 +0300

    up

---
+++ AR_Tutor/Assets/Scripts/WordComposing/WordComposingMenuControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordComposingMenuControl : GameMenu
+{
+    public override void Initialize()
+    {
+        gameName = GameName.WordComposing;
+        cardSelector = FindObjectOfType<WordComposingSelector>();
+        base.Initialize();
+    }
+}
---
+++ AR_Tutor/Assets/Scripts/WordComposing/WordComposingSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordComposingSelector : MonoBehaviour, IManageCards
+{
+    public void AddCard(GameObject _card)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Initialize(List<GameObject> _cards)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RemoveCard(GameObject _card)
+    {
+        throw new System.NotImplementedException();
+    }
+}
