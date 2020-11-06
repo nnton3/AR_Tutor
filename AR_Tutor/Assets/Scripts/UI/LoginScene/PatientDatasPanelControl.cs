6dd214e932649a4b3a51244c107a4cd71457ede1
Anton <akarnomazov@gmail.com>
2020-11-06 18:40:56 +0300

    add start game panel and patient datas panel

---
+++ AR_Tutor/Assets/Scripts/UI/LoginScene/PatientDatasPanelControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PatientDatasPanelControl : MonoBehaviour
+{
+    [SerializeField] private GameObject patinetDatasPanel, patientsDataContent, patientDataCardPref;
+    [SerializeField] private VerticalLayoutGroup patientDatasLayoutGroup;
+    [SerializeField] private Button closeBtn;
+
+    public void Initialize()
+    {
+        if (closeBtn != null) closeBtn.onClick.AddListener(() => patinetDatasPanel.SetActive(false));
+
+        Signals.AddPatientEvent.AddListener(AddPatientInDataPanel);
+    }
+
+    public void AddPatientInDataPanel(PatientData _data, string _identifier)
+    {
+        if (patientsDataContent == null) return;
+        if (patientDataCardPref == null) return;
+
+        var card = Instantiate(patientDataCardPref, patientsDataContent.transform);
+        card.GetComponent<PatientDataCard>().Initialize(_data, _identifier);
+
+        card.transform.SetAsFirstSibling();
+
+        UIInstruments.GetSizeForVerticalGroup(
+            patientDatasLayoutGroup,
+            patientsDataContent.transform.childCount,
+            card.GetComponent<RectTransform>().sizeDelta.y * card.GetComponent<RectTransform>().localScale.y);
+    }
+}
